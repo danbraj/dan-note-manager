281ddb343937380596c3700ef1062fcfcd268641
danbraj <danbraj@users.noreply.github.com>
2018-03-29 16:19:46 +0200

    Init

---
+++ app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        orginalText: 'Orginal text',
+        translatedText: 'Translated text',
+        sentence: 'Sentence',
+        playground: 'Placeholder'
+    },
+    methods: {
+
+    }
+});
